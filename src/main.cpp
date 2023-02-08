6f08fca27977c016393d942b8f6f238e67730a43
urkadurka <paolop66web@2pem.it>
2023-02-08 10:05:16 +0100

    Gestioe ACC SPI

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
+#include <main.h>
 #include <Prop_SPI.h>
+#include <ICM.h>
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,6 +20,15 @@ void setup() {
   setupMySPI(); // 2^ SPI Channel for ADA ICM20948
 }
 
+
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  ui8Generic = ICM_WhoAmI(MySPI, ICM_CS);
+  ui16Generic = getAccAxis(MySPI, zAxis);
+  Serial.printf("WhoIAm=%D\t Counter %d\t Acc Z  %d\n ",ui8Generic, ui8GenericCounter++, ui16Generic);
+  if (ui8GenericCounter>100) ui8GenericCounter =0;
+  delay(1000);
+
+
 }
